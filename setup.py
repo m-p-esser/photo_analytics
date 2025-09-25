fe2b92d96232a1d93f28047a36e53e4c95d1503d
m-p-esser <m-esser@mail.de>
2025-09-25 10:35:37 +0200

    make custom modules installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Photo-Analytics',
+    version='1.0.0',
+    url='https://github.com/m-p-esser/photo_analytics.git',
+    author='Marc-Philipp Esser',
+    author_email='m-esser@mail.de',
+    description='Description of my package',
+    packages=find_packages(),    
+)
